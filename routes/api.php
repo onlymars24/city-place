747731981b1bb614e5745672bd6865991fe3d330
onlymars24 <marsel.galimov.241@gmail.com>
2024-06-02 23:20:31 +0300

    c2

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TypeController;
 use App\Http\Controllers\PlaceController;
 
 /*
@@ -23,6 +24,8 @@ Route::middleware('auth:api')->group(function() {
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+
+Route::get('/types', [TypeController::class, 'all']);
 
 Route::get('/places', [PlaceController::class, 'all']);
 Route::get('/place', [PlaceController::class, 'one']);
