5a620221aa7b35d7a62a1fde46939cfde1de870e
onlymars24 <marsel.galimov.241@gmail.com>
2024-06-05 06:09:58 +0300

    c2

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('surname');
             $table->string('patronymic')->nullable();
+            $table->string('avatar')->nullable();
             $table->string('email')->unique();
             $table->string('password');
             $table->boolean('admin')->default(0);
